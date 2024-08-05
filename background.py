c1c2d4d3b229ae36686eaf7f75c99ae874b9c1b8
AlroyMenezes <122968039+AlroyMenezes@users.noreply.github.com>
2024-08-05 15:38:30 +0530

    Update background.py

--- background.py
+++ background.py
@@ -2,16 +2,7 @@ import streamlit as st
 import base64
 
 
-def add_bg_from_local():
-    
-    #"C:/Users/alroy/Downloads/background.jpg"
-    #"C:/Users/alroy/Downloads/2.png"
-    #"C:/Users/alroy/Downloads/mexican-coins-arrangement.jpg" 
-
-    #"C:/Users/alroy/Downloads/stacks-coins-arranged-bar-graph.jpg"
-    #"C:/Users/alroy/Downloads/gold-coin-hourglass-time-is-money-concept.jpg"
-    #"C:/Users/alroy/OneDrive/Documents/AIMIT/SEM 4/Background DNP/growth-economy-with-coins-concept.jpg"
-    #"C:/Users/alroy/OneDrive/Documents/AIMIT/SEM 4/Background DNP/table-with-finance-work-stuff-coffee-money-tablet-pen-papers.jpg"
+def add_bg_from_local(): 
     image_path = r"C:/Users/alroy/OneDrive/Documents/AIMIT/SEM 4/Background DNP/growth-economy-with-coins-concept.jpg" # Update this path to match your image path
     with open(image_path, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read()).decode()
